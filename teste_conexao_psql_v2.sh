87bae945eeb7cfa6207863441abb0c1175cd14a3
Edgar <edgar.edps@gmail.com>
2024-07-04 16:37:20 -0300

    variaveis de ambiente atualizadas nos arquivos .sh

--- teste_conexao_psql_v2.sh
+++ teste_conexao_psql_v2.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-secret_string=$(aws secretsmanager get-secret-value --secret-id MEU_SEGREDO --version-stage AWSCURRENT --query SecretString --output text)
+secret_string=$(aws secretsmanager get-secret-value --secret-id rds!db-eb7473f7-d8a6-499a-ab43-0a6763f1344b --version-stage AWSCURRENT --query SecretString --output text)
 username=$(echo $secret_string | jq -r '.username')
 password=$(echo $secret_string | jq -r '.password')
 
 # Variáveis de conexão
-DB_HOST="RDS_ENDPOINT"
+DB_HOST="db-com-secrets.cifp3umrxxtb.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
 DB_NAME="postgres"
 DB_USER=$username
